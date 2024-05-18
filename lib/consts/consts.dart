f99b19aea222350d274e313f838668f25cec954a
DooxFonggg <phongcoi1235@gmail.com>
2024-05-19 01:07:14 +0700

    start project

---
+++ lib/consts/consts.dart
@@ -0,0 +1,7 @@
+//khai báo nhanh tất cả các file dart trong const => chỉ cần export file vào main là xong
+
+export './colors.dart';
+export './images.dart';
+export './strings.dart';
+export './styles.dart';
+// export 'package:velocity_x/velocity_x.dart';
---
+++ lib/consts/strings.dart
@@ -0,0 +1,3 @@
+const appname = "F-Mart";
+const appversion = "Version 1.0.0";
+const credits = "@Baaba Devs";
